f0b51a33591ff982d6f84ea1eb48945e0ef16cc8
ipc103 <iancandysemail@gmail.com>
2016-09-22 09:28:15 -0400

    solution

---
+++ blog_posts.js
@@ -0,0 +1,5 @@
+const blogPosts = [
+  {title: 'A Cool Post', content: "Here's an awesome blog post"},
+  {title: 'Second Post', content: "Another great post"},
+  {title: 'A Third Post', content: "Isn't blogging Awesome???"},
+]
